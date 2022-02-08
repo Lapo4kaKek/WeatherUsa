2abd3770442f4013e9078376a3d507c7fa7697ac
Lapo4kaKek <97127102+Lapo4kaKek@users.noreply.github.com>
2022-02-08 19:45:42 +0300

    The final version of the lab

--- Program.cs
+++ Program.cs
@@ -2,24 +2,153 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
 namespace WeatherAnalysis
 {
-    
+    internal delegate Response CallingMethod(List<WeatherCreate> weather);
+
+    public record Response(List<string> stringline)
+    {
+        public override string ToString()
+        {
+            string str = "";
+            foreach (var item in stringline)
+            {
+                str += item+"\n"; 
+            }
+            return str;
+        }
+    }
+    class MethodAnalytics
+    {
+        public static Response Task0(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            var result = from i in weather where i.StartTime.Year is 2018 select i;
+            //var result = weather.Count(i => i.StartTime.Year is 2018);
+            resp.stringline.Add($"Task0:\n{result.Count()}");
+            resp.stringline.Add("---------------------------");
+            return resp;
+
+        }
+        public static Response Task1(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            List<string> statesUsa = new();
+            List<string> cityUsa = new();
+            for (int j = 0; j < weather.Count; j++)
+            {
+                statesUsa.Add(weather[j].State);
+                cityUsa.Add(weather[j].City);
+            }
+            IEnumerable<string> distinctStates = statesUsa.Distinct();
+            IEnumerable<string> distinctCity = cityUsa.Distinct();
+            resp.stringline.Add($"Task1\nВ датасете:{distinctStates.Count()} штатов и {distinctCity.Count()} городов");
+            resp.stringline.Add("---------------------------");
+            return resp;
+
+        }
+        public static Response Task2(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            var resulttwo = weather.Where(_ => _.StartTime.Year > 2019).GroupBy(_ => (_.Type, _.City))
+                .Where(__ => __.Key.Type == WeatherType.Rain).Select(__ => new { City = __.Key.City, Rains = __.Count() })
+                .OrderByDescending(___ => ___.Rains).Take(3);
+            resp.stringline.Add("Task2:");
+            resp.stringline.Add($"{string.Join(", ", resulttwo.Select(___ => $"{___.Rains} дождей в {___.City}"))}");
+            resp.stringline.Add("---------------------------");
+            return resp;
+        }
+        public static Response Task3(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            var resultthree = weather.GroupBy(r => r.StartTime.Year).Select(g => new
+            {
+                Year = g.Key,
+                longestSnowfall = g.OrderByDescending(_ => _.EndTime - _.StartTime).First()
+            });
+            resp.stringline.Add($"Task3:\n{string.Join(", ", resultthree.Select(a => $"{a.Year} - from {a.longestSnowfall}"))}");
+            resp.stringline.Add("---------------------------");
+            return resp;
+        }
+        public static Response Task4(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            var resultfour = weather.Where(r => r.StartTime.Year == 2019)
+                .OrderBy(r => r.StartTime).GroupBy(r => r.State).Select(g => new
+                {
+                    State = g.Key,
+                    Count = g.TakeWhile(r => r.EndTime - r.StartTime <= TimeSpan.FromHours(2)).Count()
+                });
+            resp.stringline.Add("Task4:");
+            foreach (var stateCount in resultfour)
+            {
+                resp.stringline.Add($"{stateCount.State} - {stateCount.Count} событий превышающие по длительности 2 часа");
+            }
+            resp.stringline.Add("---------------------------");
+            return resp; 
+        }
+        public static Response Task5(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            var resultfive = weather.Where(r => r.StartTime.Year is 2017 or 2018 or 2019! & r.Severity == WeatherSeverity.Severe)
+                .Select(r => new
+                {
+                    r.State,
+                    Start = r.StartTime,
+                    End = r.EndTime,
+                    Duration = r.EndTime - r.StartTime,
+                    r.City
+                }).GroupBy(r => r.State).Select(g => new
+                {
+                    State = g.Key,
+                    longestEvent = g.OrderByDescending(r => r.Duration).First()
+                }).OrderByDescending(r => r.longestEvent.Duration);
+            resp.stringline.Add("Task5:");
+            foreach (var item in resultfive)
+            {
+                resp.stringline.Add($"{item.State}-{Math.Round(item.longestEvent.Duration.TotalHours)}");
+            }
+            resp.stringline.Add("---------------------------");
+            return resp;
+        }
+        public static Response Task6(List<WeatherCreate> weather)
+        {
+            var resp = new Response(new List<string>());
+            var resultsix = weather.GroupBy(i => i.StartTime.Year).Select(i => (i.Key, i.GroupBy(l => l.Type)))
+                .Select(i => (i.Key, i.Item2.OrderBy(j => j.Count()))).ToList();
+            var mostEvents = resultsix.Select(i => (i.Key, i.Item2.Last())).
+                Select(i => (i.Key, i.Item2, i.Item2.Average(j => (j.EndTime - j.StartTime).TotalHours)));
+            var rarelyEvents = resultsix.Select(i => (i.Key, i.Item2.First()))
+                .Select(i => (i.Key, i.Item2, i.Item2.Average(j => (j.EndTime - j.StartTime).TotalHours)));
+            resp.stringline.Add("Task6:");
+            foreach(var item in mostEvents)
+            {
+                resp.stringline.Add($"{item.Key}: {item.Item2.Key.ToString()} в среднем {Math.Round(item.Item3, 3)} часов");
+            }
+            foreach(var item in rarelyEvents)
+            {
+                resp.stringline.Add($"{item.Key}: {item.Item2.Key.ToString()} в среднем {Math.Round(item.Item3,3)} чаосв");
+            }
+            return resp;
+        }
+    }
     public record WeatherCreate
     (
-        string EventId,
-        WeatherType Type, 
-        WeatherSeverity Severity,
-        DateTime StartTime, 
-        DateTime EndTime,
-        string TimeZone,
-        string AirportCode,
-        string LocationLat,
-        string LocationLng, 
-        string City, 
-        string County, 
-        string State, 
-        string ZipCode 
+        string EventId, //{ get; init; }
+        WeatherType Type, //{ get; init; }
+        WeatherSeverity Severity,// { get; init; }
+        DateTime StartTime, //{ get; init; }
+        DateTime EndTime,// { get; init; }
+        string TimeZone,// { get; init; }
+        string AirportCode,// { get; init; }
+        string LocationLat, //{ get; init; }
+        string LocationLng, //{ get; init; }
+        string City, //{ get; init; }
+        string County, //{ get; init; }
+        string State, //{ get; init; }
+        string ZipCode //{ get; init; }
     );
     public enum WeatherType
     {
@@ -43,6 +172,7 @@ namespace WeatherAnalysis
     }
     class Program
     {
+
         static void Main(string[] args)
         {
             List<WeatherCreate> weather = new();
@@ -78,77 +208,74 @@ namespace WeatherAnalysis
                 }
             }
 
-
-            
-            Console.WriteLine("Task 0:");
-            var result = from i in weather where i.StartTime.Year is 2018 select i;
-            //var result = weather.Count(i => i.StartTime.Year is 2018);
-            Console.WriteLine(result.Count());
-            Console.WriteLine("---------------------------\n");
-
-            Console.WriteLine("Task 1:");
-            List<string> statesUsa = new();
-            List<string> cityUsa = new();
-            for (int j = 0; j < weather.Count; j++)
+            //CallingMethod AnalyticsWeather = Task.Task0;
+            //AnalyticsWeather += Task.Task1;
+            //AnalyticsWeather += Task.Task2;
+            //AnalyticsWeather += Task.Task3;
+            //AnalyticsWeather += Task.Task4;
+            //AnalyticsWeather += Task.Task5;
+            //AnalyticsWeather += Task.Task6;
+            //AnalyticsWeather(weather);
+            List<CallingMethod> analyticsWeather = new List<CallingMethod>(7);
+            analyticsWeather.Add(MethodAnalytics.Task0);
+            analyticsWeather.Add(MethodAnalytics.Task1);
+            analyticsWeather.Add(MethodAnalytics.Task2);
+            analyticsWeather.Add(MethodAnalytics.Task3);
+            analyticsWeather.Add(MethodAnalytics.Task4);
+            analyticsWeather.Add(MethodAnalytics.Task5);
+            analyticsWeather.Add(MethodAnalytics.Task6);
+            // Первый способ - for
+            Stopwatch measurementOne = new Stopwatch();
+            measurementOne.Start();
+            for (int i = 0; i < analyticsWeather.Count(); i++)
             {
-                statesUsa.Add(weather[j].State);
-                cityUsa.Add(weather[j].City);
+                var line = analyticsWeather[i](weather).ToString();
+                Console.WriteLine(line);
             }
-            IEnumerable<string> distinctStates = statesUsa.Distinct();
-            IEnumerable<string> distinctCity = cityUsa.Distinct();
-            Console.WriteLine($"В датасете:{distinctStates.Count()} штатов и {distinctCity.Count()} городов");
-            Console.WriteLine("---------------------------\n");
+            measurementOne.Stop();
 
-            Console.WriteLine("Task 2:");
-            var resulttwo = weather.Where(_ => _.StartTime.Year > 2019).GroupBy(_ => (_.Type, _.City))
-                .Where(__ => __.Key.Type == WeatherType.Rain).Select(__ => new { City = __.Key.City, Rains = __.Count() })
-                .OrderByDescending(___ => ___.Rains).Take(3);
-            Console.WriteLine($"{string.Join(", ", resulttwo.Select(___ => $"{___.Rains} дождей в {___.City}"))}");
-            Console.WriteLine("----------------------------");
-
-            Console.WriteLine("Task 3:");
-            var resultthree = weather.GroupBy(r => r.StartTime.Year).Select(g => new
+            // Второй способ - Parallel.ForEach
+            Stopwatch measurementTwo = new Stopwatch();
+            measurementTwo.Start();
+            Parallel.ForEach(analyticsWeather, i =>
             {
-                Year = g.Key,
-                longestSnowfall = g.OrderByDescending(_ => _.EndTime - _.StartTime).First()
+                var line = i(weather).ToString();
+                //Console.Write(line);
             });
-            Console.WriteLine($"{string.Join(", ", resultthree.Select(a => $"{a.Year} - from {a.longestSnowfall}"))}");
-            Console.WriteLine("----------------------------");
+            measurementTwo.Stop();
 
-            Console.WriteLine("Task 4:");
-            var resultfour = weather.Where(r => r.StartTime.Year == 2019)
-                .OrderBy(r => r.StartTime).GroupBy(r => r.State).Select(g => new
-                {
-                    State = g.Key,
-                    Count = g.TakeWhile(r => r.EndTime - r.StartTime <= TimeSpan.FromHours(2)).Count()
-                });
-            foreach (var stateCount in resultfour)
+            // Третий способ - Task WaiAll
+            Stopwatch measurementThree = new Stopwatch();
+            measurementThree.Start();
+            Task[] tasks = new Task[7]
             {
-                Console.WriteLine($"{stateCount.State} - {stateCount.Count} событий превышающие по длительности 2 часа");
-            }
-            Console.WriteLine("---------------------------");
+                new Task(() => analyticsWeather[0](weather).ToString()),
+                new Task(() => analyticsWeather[1](weather).ToString()),
+                new Task(() => analyticsWeather[2](weather).ToString()),
+                new Task(() => analyticsWeather[3](weather).ToString()),
+                new Task(() => analyticsWeather[4](weather).ToString()),
+                new Task(() => analyticsWeather[5](weather).ToString()),
+                new Task(() => analyticsWeather[6](weather).ToString())
+            };
+            foreach (var item in tasks)
+                item.Start();
+            Task.WaitAll(tasks); // ожидаем завершения задач
+            measurementThree.Stop();
 
-            Console.WriteLine("Task 5:");
-            var resultfive = weather.Where(r => r.StartTime.Year is 2017 or 2018 or 2019! & r.Severity == WeatherSeverity.Severe)
-                .Select(r => new
-                {
-                    r.State,
-                    Start = r.StartTime,
-                    End = r.EndTime,
-                    Duration = r.EndTime - r.StartTime,
-                    r.City
-                }).GroupBy(r => r.State).Select(g => new
-                {
-                    State = g.Key,
-                    longestEvent = g.OrderByDescending(r => r.Duration).First()
-                }).OrderByDescending(r => r.longestEvent.Duration);
-            foreach (var item in resultfive)
-            {
-                Console.WriteLine($"{item.State}-{Math.Round(item.longestEvent.Duration.TotalHours)}");
-            }
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine($"Метод for занимает:{measurementOne.ElapsedMilliseconds} миллисекунд\n");
+            Console.WriteLine($"Метод Parallel.ForEach занимает:{measurementTwo.ElapsedMilliseconds} миллисекунд\n");
+            Console.WriteLine($"Метод Tasks.WaitAll занимает:{measurementThree.ElapsedMilliseconds} миллисекунд\n");
 
-
+            double effectiveness;
+            List<double> list = new List<double>(3) { 0, 0,  0 };
+            list[0] = measurementOne.ElapsedMilliseconds;
+            list[1] = measurementTwo.ElapsedMilliseconds;
+            list[2] = measurementThree.ElapsedMilliseconds;
+            double max = list.Max();
+            double min = list.Min();
+            effectiveness = (max * 100) / min - 100;
+            Console.WriteLine($"Самый быстрый эффективнее самого медленного примерно на {(int)effectiveness}%");
         }
     }
 }
